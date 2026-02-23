6bf9945bf014b14d1d061beb201b046042bc75d4
Giannin <contact@giannin.ch>
2026-02-23 14:09:05 +0100

    chore: init

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = Development flake for ...;
+
+  inputs = {
+    nixpkgs.url = github:nixos/nixpkgs/nixos-25.11;
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      x86 = x86_64-linux;
+      pkgs = nixpkgs.legacyPackages.;
+    in
+    {
+      devShells..default = pkgs.mkShellNoCC {
+        packages = with pkgs; [
+          curl
+        ];
+
+        shellHook = ''
+          git config --local core.hooksPath .githooks/
+        '';
+
+        # Environment Variables
+        ENV_VAR = ;
+      };
+    };
+}
+
